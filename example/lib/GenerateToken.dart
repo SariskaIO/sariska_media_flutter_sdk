7de7c8afc5bfb009ce86cae98b49cd5256b5fc7d
Gaurav Poudel <gauravpaudel2013@gmail.com>
2023-12-11 14:47:19 +0545

    permission request added

--- example/lib/GenerateToken.dart
+++ example/lib/GenerateToken.dart
@@ -20,17 +20,14 @@ Future<String> generateToken() async {
     print('Response body: ${response.body}');
 
     if (response.statusCode == 200) {
-      // If the server returned a 200 OK response, parse the JSON.
       var responseBody = jsonDecode(response.body);
       print(responseBody);
       return responseBody['token'];
     } else {
-      // If the server did not return a 200 OK response, throw an exception.
       throw Exception(
           'Failed to generate token. Status code: ${response.statusCode}');
     }
   } catch (e) {
-    // Handle exceptions thrown during the request (e.g., network issues).
     print('Exception during request: $e');
     throw Exception('Failed to generate token. Check your network connection.');
   }
